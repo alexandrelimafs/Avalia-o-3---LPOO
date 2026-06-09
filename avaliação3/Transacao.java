d5173d47553e41bd1fa829e18c3a4f9e1feae282
alexandrelimafs <alfs5@discente.ifpe.edu.br>
2026-06-09 19:41:03 -0300

    Fix constructor and add missing getter for tipo

--- avaliação3/Transacao.java
+++ avaliação3/Transacao.java
@@ -1,20 +1,16 @@
 package avaliacao3;
 
-// Classe Transação
 public class Transacao {
-    //Atributos privados
     private String descricao;
     private double valor;
     private int tipo;
     
-    // Construtor que que recebe e inicializa os atributos
     public Transacao(String descricao, double valor, int tipo) {
         this.descricao = descricao;
         this.valor = valor;
-        this.tipo = tipo; // Corrigi o this.tipo, antes não inicializava o atributo tipo no construtor.
+        this.tipo = tipo;
     }
     
-    // Métodos Getters pra ler os dados de fora (public)
     public String getDescricao() { 
         return descricao; 
     }
@@ -23,9 +19,8 @@ public class Transacao {
         return valor; 
     }
     
-    public int getTipo() { // Corrigi: criei o getter do tipo que estava ausente.
+    public int getTipo() {
         return tipo;
     }
     
-    // E removi o getData() que a questão não pedia.
 }
