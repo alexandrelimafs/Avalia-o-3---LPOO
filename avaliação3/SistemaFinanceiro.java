a8e1a74e53e11491e91c7e391b4d5f186ce59710
alexandrelimafs <alfs5@discente.ifpe.edu.br>
2026-06-09 19:45:11 -0300

    Update SistemaFinanceiro.java

--- avaliação3/SistemaFinanceiro.java
+++ avaliação3/SistemaFinanceiro.java
@@ -5,20 +5,16 @@ import java.util.List;
 
 public class SistemaFinanceiro {
     
-    // Corrigi: antes não utilizava uma lista de objetos Transacao (ArrayList), que era exigido na questão.
     private List<Transacao> listaDeTransacao = new ArrayList<>();
     
-    // Corrigi: antes o construtor não existia ou estava incompleto.
     public SistemaFinanceiro() {
         this.listaDeTransacao = new ArrayList<>();
     }
     
-    // Corrigi: implementei o método adicionar para inserir objetos Transacao na lista.
     public void adicionar(Transacao t) {
         this.listaDeTransacao.add(t);
     }
     
-    // Corrigi: o metodo listarExtrato estava com lógica errada e não percorria a lista de transações corretamente.
     public void listarExtrato() {
         System.out.println("------------------");
         System.out.println("EXTRATO:");
